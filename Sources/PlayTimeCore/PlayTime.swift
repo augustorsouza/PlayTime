f285c0f0a48f5e56233a9ce6e1d4e581e54fda01
Augusto Souza <augustorsouza@gmail.com>
2018-03-31 09:48:34 -0300

    Adding logic to create a playground file

--- Sources/PlayTimeCore/PlayTime.swift
+++ Sources/PlayTimeCore/PlayTime.swift
@@ -1,13 +1,75 @@
 import Foundation
 
 public final class PlayTime {
-    private let arguments: [String]
+    private enum Platform: String {
+        case ios, osx
+
+        init(arguments: [String]) {
+            self = arguments.contains("osx") ? .osx : .ios
+        }
+
+        var contentsXcPlayground: String {
+            return CONTENTS_XCPLAYGROUND_TEMPLATE.replacingOccurrences(of: "#{PLATFORM}", with: rawValue)
+        }
+
+        var contentsSwift: String {
+            return IMPORT_TEMPLATE.replacingOccurrences(of: "#{FRAMEWORK}", with: framework)
+        }
+
+        private var framework: String {
+            switch self {
+            case .ios:
+                return "UIKit"
+            case .osx:
+                return "Cocoa"
+            }
+        }
+    }
+
+    private let platform: Platform
+
+    private var playgroundPath: String {
+        let now = Date()
+        return TMP_PATH_TEMPLATE.replacingOccurrences(of: "#{PLAYGROUND_FILE_NAME}", with: formatter.string(from: now))
+    }
+
+    private var formatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        return formatter
+    }
 
     public init(arguments: [String] = CommandLine.arguments) {
-        self.arguments = arguments
+        self.platform = Platform(arguments: arguments)
     }
 
     public func run() throws {
-        print("Hello world")
+        do {
+            try FileManager.default.createDirectory(atPath: playgroundPath, withIntermediateDirectories: true, attributes: nil)
+        } catch let error {
+            fatalError("Unable to create the playground directory. Error: \(error)")
+        }
+
+        let tmpUrl = URL(fileURLWithPath: playgroundPath, isDirectory: true)
+
+        let contentsXcPlaygrounFilePath = tmpUrl
+            .appendingPathComponent(CONTENTS_XCPLAYGROUND_FILENAME)
+
+        do {
+            try platform.contentsXcPlayground.write(to: contentsXcPlaygrounFilePath, atomically: false, encoding: .utf8)
+        } catch let error {
+            fatalError("Unable to write to the xcplayground file. Error: \(error)")
+        }
+
+        let contentsSwiftFilePath = tmpUrl
+            .appendingPathComponent(CONTENTS_SWIFT_FILENAME)
+
+        do {
+            try platform.contentsSwift.write(to: contentsSwiftFilePath, atomically: false, encoding: .utf8)
+        } catch let error {
+            fatalError("Unable to write to the xcplayground file. Error: \(error)")
+        }
+
+    }
     }
 }
