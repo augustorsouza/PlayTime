db7c837d670ed6e126ba26d7e72132bfb66eefe6
Augusto Souza <augustorsouza@gmail.com>
2018-03-31 09:48:50 -0300

    After finished, we must open the generated playground file

--- Sources/PlayTimeCore/PlayTime.swift
+++ Sources/PlayTimeCore/PlayTime.swift
@@ -70,6 +70,16 @@ public final class PlayTime {
             fatalError("Unable to write to the xcplayground file. Error: \(error)")
         }
 
+        shell("open", playgroundPath)
     }
+
+    @discardableResult
+    private func shell(_ args: String...) -> Int32 {
+        let task = Process()
+        task.launchPath = "/usr/bin/env"
+        task.arguments = args
+        task.launch()
+        task.waitUntilExit()
+        return task.terminationStatus
     }
 }
